1712d74c495749b70b254846c3569b5e1fa84e75
douliyou <douliyou@live.cn>
2021-12-21 20:42:09 +0800

    类名以驼峰法命名

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 
 
 # 用于中间环节对数据的传递
-class global_var:
+class GlobalVar:
     name = None
 
 
 def set_name(name):
-    global_var.name = name
+    GlobalVar.name = name
 
 
 def get_name():
-    return global_var.name
+    return GlobalVar.name
